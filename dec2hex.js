71378b9500239eaa561f826fab2f216aa7cf54bf
Dylan <dylan.nunns@gmail.com>
2017-11-22 11:41:46 -0500

    Initial Commit

---
+++ dec2hex.js
@@ -0,0 +1,11 @@
+/**
+ * Convert the Decimal value to its Hexadecimal counterpart
+ */
+function decimal_to_hexadecimal(decimal = 0) {
+    const _hexadecimal = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+    for (var hex = []; 0 != decimal;) {
+        hex.unshift(_hexadecimal[decimal % 16]);
+        decimal = Math.floor(decimal / 16);
+    }
+    return hex.join('');
+}
